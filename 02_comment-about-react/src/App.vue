d61959a4c34da49ea4e716c72ea64303311e4239
RyenToretto <18273727925@163.com>
2019-02-18 22:55:53 +0800

    comment about react

--- 02_comment-about-react/src/App.vue
+++ 02_comment-about-react/src/App.vue
@@ -1,14 +1,24 @@
 <template>
+  <div>
     <Header></Header>
+    <div class="container">
+      <AddComment></AddComment>
+      <Comments></Comments>
+    </div>
+  </div>
 </template>
 
 <script>
   import Header from "./components/Header"
+  import AddComment from "./components/AddComment"
+  import Comments from "./components/Comments"
   
   export default {
     name: 'App',
     components: {
       Header,
+      AddComment,
+      Comments
     },
     data (){
       return {
--- 02_comment-about-react/src/main.js
+++ 02_comment-about-react/src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 
-import { Button } from 'mint-ui';
-Vue.component(Button.name, Button);        // mt-button
-
 new Vue({
   el: '#app',
   components: {
